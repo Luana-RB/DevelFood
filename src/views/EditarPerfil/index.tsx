de094b0aca32c90b71c77ed88bb7c98726ec2d95
Luana Reginato Bassanesi <luana.bassanesi@develcode.com.br>
2024-05-16 15:32:15 -0300

    José/Validações edição perfil

--- src/views/EditarPerfil/index.tsx
+++ src/views/EditarPerfil/index.tsx
@@ -11,6 +11,8 @@ import DataCell from './components/DataCell';
 import {UserData} from '../../types/userData';
 import {getUserData, putUser} from '../../services/api/users';
 import {getAddressById, patchUserAddress} from '../../services/api/address';
+import {Errors} from '../../types/errors';
+import {ErrorText} from '../../components/Input/styles';
 
 const EditarPerfil: React.FC = () => {
   const {userData, userAddress, storeAddress, storeData} = useUser();
@@ -33,6 +35,7 @@ const EditarPerfil: React.FC = () => {
   const [imagePath, setImagePath] = useState(
     require('../../../assets/images/notFound.png'),
   );
+  const [errors, setErrors] = useState<Errors>({});
 
   useEffect(() => {
     const path = userData?.image;
@@ -41,37 +44,101 @@ const EditarPerfil: React.FC = () => {
   }, [userData]);
 
   async function handleSubmit() {
-    const newUser: UserData = {
-      id: userData?.id,
-      password: userData?.password!,
-      firstName,
-      lastName,
-      email,
-      image: userData?.image ?? '',
-      cpf,
-      phone,
-      address: [
-        {
-          addressName: addressName,
-          addressId: userAddress?.id,
-          cep: cep,
-          street: street,
-          state: state,
-          city: city,
-          neighbourhood: neighbourhood,
-          number: number,
-        },
-      ],
-    };
-    const userResponse = await putUser(newUser);
-    const addressResponse = await patchUserAddress(newUser);
-    if (userResponse && addressResponse) {
-      Alert.alert('Salvo com sucesso');
-      const fetchedUserData = await getUserData();
-      storeData(fetchedUserData);
-      const fetchedUserAddress = await getAddressById();
-      storeAddress(fetchedUserAddress);
-    } else Alert.alert('Falha ao salvar');
+    const newErrors = validateForm();
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+      const newUser: UserData = {
+        id: userData?.id,
+        password: userData?.password!,
+        firstName,
+        lastName,
+        email,
+        image: userData?.image ?? '',
+        cpf,
+        phone,
+        address: [
+          {
+            addressName: addressName,
+            addressId: userAddress?.id,
+            cep: cep,
+            street: street,
+            state: state,
+            city: city,
+            neighbourhood: neighbourhood,
+            number: number,
+          },
+        ],
+      };
+      const userResponse = await putUser(newUser);
+      const addressResponse = await patchUserAddress(newUser);
+      if (userResponse && addressResponse) {
+        Alert.alert('Salvo com sucesso');
+        const fetchedUserData = await getUserData();
+        storeData(fetchedUserData);
+        const fetchedUserAddress = await getAddressById();
+        storeAddress(fetchedUserAddress);
+      } else Alert.alert('Falha ao salvar');
+    }
+  }
+
+  function validateForm() {
+    let errors: Errors = {};
+    const telefoneError = validateTelefone();
+    if (telefoneError) errors.cellphone = telefoneError;
+    const apelidoError = validateApelido();
+    const cepError = validateCep();
+    const ruaError = validateRua();
+    const cidadeError = validateCidade();
+    const bairroError = validateBairro();
+    const estadoError = validateEstado();
+    const numError = validateNum();
+
+    if (apelidoError) errors.apelido = apelidoError;
+    if (cepError) errors.cep = cepError;
+    if (ruaError) errors.rua = ruaError;
+    if (cidadeError) errors.cidade = cidadeError;
+    if (bairroError) errors.bairro = bairroError;
+    if (estadoError) errors.estado = estadoError;
+    if (numError) errors.num = numError;
+
+    return errors;
+  }
+
+  function validateApelido() {
+    if (!addressName) return 'Insira um apelido';
+  }
+
+  function validateCep() {
+    if (/\d{5}-\d{3}/.test(cep)) return undefined;
+    return 'Insira um CEP';
+  }
+
+  function validateRua() {
+    if (!street) return 'Insira uma rua';
+  }
+
+  function validateCidade() {
+    if (!city) return 'Insira uma cidade';
+  }
+
+  function validateBairro() {
+    if (!neighbourhood) return 'Insira um bairro';
+  }
+
+  function validateEstado() {
+    if (/[A-Za-z]{2}/.test(state)) return undefined;
+    return 'Insira um estado';
+  }
+
+  function validateNum() {
+    if (/^[0-9]+$/.test(number)) return undefined;
+    return 'Insira um número';
+  }
+
+  function validateTelefone() {
+    if (!/\+\d{2} \d{5}-\d{4}/.test(phone)) return 'Telefone inválido';
+    return undefined;
   }
 
   return (
@@ -109,6 +176,7 @@ const EditarPerfil: React.FC = () => {
           value={phone}
           handleChange={setPhone}
         />
+        {errors.cellphone && <ErrorText>{errors.cellphone}</ErrorText>}
         <DataCell
           icon="email-outline"
           title="E-mail"
@@ -124,6 +192,7 @@ const EditarPerfil: React.FC = () => {
             value={addressName}
             handleChange={setAddressName}
           />
+          {errors.apelido && <ErrorText>{errors.apelido}</ErrorText>}
           <DataInputCell
             icon="map-marker-outline"
             title="CEP"
@@ -131,6 +200,7 @@ const EditarPerfil: React.FC = () => {
             small={true}
             handleChange={setCep}
           />
+          {errors.cep && <ErrorText>{errors.cep}</ErrorText>}
         </View>
         <DataInputCell
           icon="map-marker-outline"
@@ -138,18 +208,21 @@ const EditarPerfil: React.FC = () => {
           value={street}
           handleChange={setStreet}
         />
+        {errors.rua && <ErrorText>{errors.rua}</ErrorText>}
         <DataInputCell
           icon="map-marker-outline"
           title="Cidade"
           value={city}
           handleChange={setCity}
         />
+        {errors.cidade && <ErrorText>{errors.cidade}</ErrorText>}
         <DataInputCell
           icon="map-marker-outline"
           title="Bairro"
           value={neighbourhood}
           handleChange={setNeighbourhood}
         />
+        {errors.bairro && <ErrorText>{errors.bairro}</ErrorText>}
         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
           <DataInputCell
             icon="map-marker-outline"
@@ -158,6 +231,7 @@ const EditarPerfil: React.FC = () => {
             value={state}
             handleChange={setState}
           />
+          {errors.estado && <ErrorText>{errors.estado}</ErrorText>}
           <DataInputCell
             icon="map-marker-outline"
             title="Número"
@@ -165,6 +239,7 @@ const EditarPerfil: React.FC = () => {
             value={number}
             handleChange={setNumber}
           />
+          {errors.num && <ErrorText>{errors.num}</ErrorText>}
         </View>
         <Button text="Salvar" handleSubmit={handleSubmit} />
         <View style={{height: 100}} />
